f3cd593d11cd8e69389a44985179d15690f198b7
Anara Imasheva <imasheva.anara1@gmail.com>
2020-05-06 19:59:41 -0700

    First steps for creating POM
    Added 3 test classes
    1.Base parent
    2. Created Data and Locators classew

---
+++ src/test/java/BaseUI.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseUI {
+    WebDriver driver;
+    String mainUrl = "https://romanceabroad.com/";
+
+
+    @BeforeMethod
+    public void setUp(){
+
+        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        driver = new ChromeDriver();
+        driver.get(mainUrl);
+
+    }
+
+
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+
+
+}
+
