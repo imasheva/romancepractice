81ba1e4640a8de3a2749b8630a02cfe981d59716
Anara Imasheva <imasheva.anara1@gmail.com>
2020-05-11 15:02:16 -0700

    add UkraineTourtest class

---
+++ src/test/java/UkraineTourTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class UkraineTourTest extends BaseUI {
+
+    @Test
+    public void testUkraineTourPage(){
+
+        //open page add assertion for url and title
+    }
+}
